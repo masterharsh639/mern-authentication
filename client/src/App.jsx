f42506752945b7caf57e5a194f6c42390cfb05d8
harshmishraskugal <harshmishra@skugal.com>
2023-10-09 13:39:08 +0530

    Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import Home from './pages/Home';
 
 const App = () => {
-  return (
-    <div className="text-red-200">App</div>
-  )
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/about" element={<About />}></Route>
+      <Route path="/signup" element={<SignUp />}></Route>
+      <Route path="/signin" element={<SignIn />}></Route>
+      <Route path="/profile" element={<Profile />}></Route>
+    </Routes>
+  </BrowserRouter>
 }
-
 export default App
