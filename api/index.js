b58f836b954c29e6ed9ad4b316871943c5389181
harshmishraskugal <harshmishra@skugal.com>
2023-10-10 23:22:18 +0530

    mongo connection

--- api/index.js
+++ api/index.js
@@ -1,7 +1,24 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const db = process.env.MONGO_URI;
+
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
 app.listen(3000, () => {
-  console.log("Server is running on PORT 3000");
+  console.log("hiiiii Server is running on PORT 3000");
 });
